b6823df37155a638380a6352108a063ad4f834d5
Malimar555132 <jamesoconnor132@gmail.com>
2026-04-03 04:44:18 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,9 @@
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
-    radio.sendNumber(2)
+    if (GHOST_CURSED == 1) {
+        radio.sendNumber(4)
+    } else {
+        radio.sendNumber(2)
+    }
 })
 radio.onReceivedNumber(function (receivedNumber) {
     hit_a_block_for_powerup(receivedNumber)
@@ -9,10 +13,33 @@ controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
     radio.sendNumber(1)
 })
 function attacked (num: number) {
-    if (num == 11) {
-    	
-    } else if (num == 12) {
-    	
+    if (num == 12) {
+        // 1= your buttons are now reversed
+        // 
+        // 
+        // need to add a timer to take off 10 secons!
+        GHOST_CURSED = 1
+        ghost_effect_how_long_lasts = game.runtime() + 10000
+    }
+}
+function hit_a_block_for_powerup (num: number) {
+    if (num == 10) {
+        music.play(music.melodyPlayable(music.baDing), music.PlaybackMode.InBackground)
+        if (has_item_already == false) {
+            spin_counter = randint(16, 18)
+            roulette_index_on_counter = 0
+            for (let index = 0; index < spin_counter; index++) {
+                if (roulette_index_on_counter == Item_Array.length) {
+                    roulette_index_on_counter = 0
+                }
+                Item.setImage(Item_Array[roulette_index_on_counter])
+                music.play(music.createSoundEffect(WaveShape.Square, 3188, 2921, 255, 0, 100, SoundExpressionEffect.None, InterpolationCurve.Curve), music.PlaybackMode.InBackground)
+                pause(100)
+                roulette_index_on_counter += 1
+            }
+            has_item_already = true
+            Item_holding = (roulette_index_on_counter - 1 + Item_Array.length) % Item_Array.length
+        }
     }
 }
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
@@ -41,57 +68,68 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     }
 })
 controller.down.onEvent(ControllerButtonEvent.Released, function () {
-    radio.sendNumber(5)
+    if (GHOST_CURSED == 1) {
+        radio.sendNumber(3)
+    } else {
+        radio.sendNumber(5)
+    }
 })
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
-    radio.sendNumber(8)
+    if (GHOST_CURSED == 1) {
+        radio.sendNumber(6)
+    } else {
+        radio.sendNumber(8)
+    }
 })
 controller.right.onEvent(ControllerButtonEvent.Released, function () {
-    radio.sendNumber(7)
+    if (GHOST_CURSED == 1) {
+        radio.sendNumber(9)
+    } else {
+        radio.sendNumber(7)
+    }
 })
 controller.left.onEvent(ControllerButtonEvent.Released, function () {
-    radio.sendNumber(9)
+    if (GHOST_CURSED == 1) {
+        radio.sendNumber(7)
+    } else {
+        radio.sendNumber(9)
+    }
 })
 controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
-    radio.sendNumber(6)
+    if (GHOST_CURSED == 1) {
+        radio.sendNumber(8)
+    } else {
+        radio.sendNumber(6)
+    }
 })
 controller.up.onEvent(ControllerButtonEvent.Released, function () {
-    radio.sendNumber(3)
+    if (GHOST_CURSED == 1) {
+        radio.sendNumber(5)
+    } else {
+        radio.sendNumber(3)
+    }
 })
 controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
-    radio.sendNumber(4)
-})
-function hit_a_block_for_powerup (num: number) {
-    if (num == 10) {
-        music.play(music.melodyPlayable(music.baDing), music.PlaybackMode.InBackground)
-        if (has_item_already == false) {
-            spin_counter = randint(16, 18)
-            roulette_index_on_counter = 0
-            for (let index = 0; index < spin_counter; index++) {
-                if (roulette_index_on_counter == Item_Array.length) {
-                    roulette_index_on_counter = 0
-                }
-                Item.setImage(Item_Array[roulette_index_on_counter])
-                music.play(music.createSoundEffect(WaveShape.Square, 3188, 2921, 255, 0, 100, SoundExpressionEffect.None, InterpolationCurve.Curve), music.PlaybackMode.InBackground)
-                pause(100)
-                roulette_index_on_counter += 1
-            }
-            has_item_already = true
-            Item_holding = (roulette_index_on_counter - 1 + Item_Array.length) % Item_Array.length
-        }
+    if (GHOST_CURSED == 1) {
+        radio.sendNumber(2)
+    } else {
+        radio.sendNumber(4)
     }
-}
+})
+let time_got_affected_by_gost_effect = 0
 let roulette_index_on_counter = 0
 let spin_counter = 0
 let has_item_already = false
+let ghost_effect_how_long_lasts = 0
+let GHOST_CURSED = 0
 let Item: Sprite = null
 let Item_empty: Image = null
 let Item_Array: Image[] = []
 let enemy_radio = 0
-let Item_holding = 0
 let your_radio = 0
-your_radio = 1
+let Item_holding = 0
 Item_holding = -1
+your_radio = 1
 enemy_radio = 3
 radio.setGroup(your_radio)
 Item_Array = [assets.image`myImage2`, assets.image`myImage1`, assets.image`myImage0`]
@@ -122,3 +160,15 @@ Item_empty = img`
     ..d11111111111111111111d..
     `
 Item = sprites.create(Item_empty, SpriteKind.Player)
+game.onUpdateInterval(100, function () {
+    if (GHOST_CURSED == 1) {
+        time_got_affected_by_gost_effect = game.runtime()
+        if (time_got_affected_by_gost_effect >= ghost_effect_how_long_lasts) {
+            // 1= your buttons are now reversed
+            // 
+            // 
+            // need to add a timer to take off 10 secons!
+            GHOST_CURSED = 0
+        }
+    }
+})
